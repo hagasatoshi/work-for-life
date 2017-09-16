e09c0ac8df1b4fdf8cc14f2d04e5110642f1f8f6
PTMBR028 <shaga@pasonatquila.com>
2017-09-16 10:23:48 +0900

    layoutファイルをerbからslimに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
+  get  '*unmatched_route', to: 'application#redirect_404', format: false
 end
