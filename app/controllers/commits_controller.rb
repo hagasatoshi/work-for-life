efe96698fb6a46f5aa9812d1d0c0dda6f22376e5
PTMBR028 <shaga@pasonatquila.com>
2017-09-17 17:54:02 +0900

    Circleの大きさと位置を動的に決定するロジックを実装

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,20 +1,33 @@
 class CommitsController < ApplicationController
+  before_action :set_service, :set_commit_counts, :set_max_count
+
   def index
+  end
+
+  private
+  def set_service
     token = session[:access_token]
     raise ArgumentError unless token.present?
 
     commits = CommitsRetrieveService
-                   .new(token: token)
-                   .commits(
-                       index_params[:org_id], index_params[:repo_id],
-                       index_params[:start_date], index_params[:end_date]
-                   )
-    @commit_count = CommitsCalculateService
+                  .new(token: token)
+                  .commits(
+                      index_params[:org_id], index_params[:repo_id],
+                      index_params[:start_date], index_params[:end_date]
+                  )
+    @calclate = CommitsCalculateService
                         .new(commits: commits)
                         .calculate
   end
 
-  private
+  def set_commit_counts
+    @commit_counts = @calclate.commit_counts
+  end
+
+  def set_max_count
+    @max_count = @calclate.max_count
+  end
+
   def index_params
     params.permit(:org_id, :repo_id, :start_date, :end_date)
   end
--- app/services/commits_calculate_service.rb
+++ app/services/commits_calculate_service.rb
@@ -1,32 +1,46 @@
 class CommitsCalculateService
   def initialize(args=nil)
     @commits = args[:commits]
-    @commit_count = init_commit_count
   end
 
   def calculate
+    @commit_counts = init_commit_count
     @commits.each do |commit|
-      sum_up!(commit)
+      sum_up(commit)
     end
-    @commit_count
+    self
+  end
+
+  def commit_counts
+    @commit_counts
+  end
+
+  def max_count
+    count = 0
+    Constants::DOW.each do |dow|
+      Constants::TIME_ARRAY.each do |time_desc|
+        count = (@commit_counts[dow][time_desc] > count ? @commit_counts[dow][time_desc] : count)
+      end
+    end
+    count
   end
 
   private
   def init_commit_count
-    commit_count = {}
+    commit_counts = {}
     Constants::DOW.each do |dow|
-      commit_count[dow] = {}
+      commit_counts[dow] = {}
       Constants::TIME_ARRAY.each do |time_desc|
-        commit_count[dow][time_desc] = 0
+        commit_counts[dow][time_desc] = 0
       end
     end
-    commit_count
+    commit_counts
   end
 
-  def sum_up!(commit)
+  def sum_up(commit)
     dow = commit[:date].strftime('%A')
     commit_time = commit[:date].strftime('%H').to_i
-    @commit_count[dow][Constants::TIME_ARRAY[commit_time]] += 1
+    @commit_counts[dow][Constants::TIME_ARRAY[commit_time]] += 1
   end
 
 end
